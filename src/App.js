feab33891fdbcb94d720a60db244acc1ee2aa9f9
Shanzzz <shahnazafreen9849@gmail.com>
2023-07-10 22:06:24 -0400

    added pdf

--- src/App.js
+++ src/App.js
@@ -144,7 +144,11 @@ function App() {
             </div>
             <div class="certifications">
                 <h2>Certification</h2>
-
+                <li>
+                        Scrum Master 6.0
+                        <img class="scrummaster" src="images/scrumsafe-icon.png" alt="Scrum Master"/>
+                        <a href="pdf/scrumcertification.pdf">Click here</a>
+                    </li>
             </div>
         </section>
 
